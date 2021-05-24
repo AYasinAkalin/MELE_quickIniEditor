3d880c0a08ffdea15602a88007db1a05cd160a1d
A. Yasin Akalın <ayasinakalin@gmail.com>
2021-05-24 13:33:11 +0300

    Update colors.py  - Add light yellow and bold, and auto test function

--- colors.py
+++ colors.py
@@ -3,6 +3,7 @@ Credits: https://stackoverflow.com/a/54955094"""
 from os import system
 system("")
 
+
 class Style:
     """A very basic class ontaining string definitions of some of the
     ASCII color codes.
@@ -19,11 +20,13 @@ class Style:
     GREEN = '\033[32m'
     LGREEN = '\033[92m'
     YELLOW = '\033[33m'
+    LYELLOW = '\033[93m'
     BLUE = '\033[34m'
     MAGENTA = '\033[35m'
     CYAN = '\033[36m'
     WHITE = '\033[37m'
     UNDERLINE = '\033[4m'
+    BOLD = '\033[1m'
     RESET = '\033[0m'
 
 
@@ -34,9 +37,15 @@ def testColors():
     print(Style.GREEN     + "GREEN"     + Style.RESET, "Hello")  # noqa: E221
     print(Style.LGREEN    + "LGREEN"    + Style.RESET, "Hello")  # noqa: E221
     print(Style.YELLOW    + "YELLOW"    + Style.RESET, "Hello")  # noqa: E221
+    print(Style.LYELLOW   + "LYELLOW"   + Style.RESET, "Hello")  # noqa: E221
     print(Style.BLUE      + "BLUE"      + Style.RESET, "Hello")  # noqa: E221
     print(Style.MAGENTA   + "MAGENTA"   + Style.RESET, "Hello")  # noqa: E221
     print(Style.CYAN      + "CYAN"      + Style.RESET, "Hello")  # noqa: E221
     print(Style.WHITE     + "WHITE"     + Style.RESET, "Hello")  # noqa: E221
-    print(Style.UNDERLINE + "UNDERLINE" + Style.RESET, "Hello")
+    print(Style.UNDERLINE + "UNDERLINE" + Style.RESET, "Hello")  # noqa: E221
+    print(Style.BOLD      + "BOLD"      + Style.RESET, "Hello")  # noqa: E221
     print(Style.RESET     + "RESET"     + Style.RESET, "Hello")  # noqa: E221
+
+
+if __name__ == "__main__":
+    testColors()
