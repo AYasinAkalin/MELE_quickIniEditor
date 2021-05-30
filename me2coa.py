e3f926f706fec51472e45a3860d38fcbee4d862c
A. Yasin Akalın <ayasinakalin@gmail.com>
2021-05-31 01:03:13 +0300

    Fix FileNotFoundError in gamelanguage.py  - Fixes #17

--- me2coa.py
+++ me2coa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """me2coa.py"""
-__version__ = "1.0.2"
+__version__ = "1.1.1"
 __author__ = "A. Yasin Akalın"
 __credits__ = ""
 __license__ = "GNU General Public License v3.0"
@@ -35,11 +35,25 @@ def confirmcoafile(gameLangObj):
     inf = color.LYELLOW + "Info!" + color.RESET
     succ = color.LGREEN + "Success!" + color.RESET
     warn = color.YELLOW + "Warning!" + color.RESET
+    err = color.RED + "Error!" + color.RESET
 
-    print(
-        inf,
-        "Mass Effect Legendary Edition's language setting is:",
-        color.BOLD + language + color.RESET)
+    if gameLangObj.isConfigFileUsed():
+        print(
+            inf,
+            "Mass Effect Legendary Edition's language setting is:",
+            color.BOLD + language + color.RESET)
+    else:
+        # At this point an error occurred before when
+        # reading LauncherConfig.cfg. A default language is chosen
+        # Inform the user about this situation.
+        print(
+            err,
+            "Game's language setting from configuration file couldn't read.")
+        print(
+            inf,
+            "Default language is selected as game's language:",
+            color.BOLD + language + color.RESET
+            )
     print(
         inf,
         "Selected Mass Effect 1 Coalesced file (You CANNOT change this):",
@@ -59,7 +73,7 @@ def confirmcoafile(gameLangObj):
         while selection not in range(1, 6):
             if selection != "Shepard must choose an ending!":
                 print(warn, "Unrecognized answer. Your response should be between 1-5.")
-            _input = input("Select on of the file from the table [1|2|3|4|5]: ")
+            _input = input("Select on of the file from the table for [1|2|3|4|5]: ")
             if _input.isdecimal():
                 selection = int(_input)
             else:
@@ -75,14 +89,17 @@ def confirmcoafile(gameLangObj):
 
 def printtable():
     coalFiles = constants.massEffect2CoalFiles
-    style = (color.UNDERLINE, color.RESET)
+    style = (color.UNDERLINE, color.UNDERLINE + color.BOLD, color.RESET)
     headers = ("File Name", "Selection")
     # Print Headers
     print("{}{:46}{}".format(
-        style[0], "", style[1])
+        style[0], "", style[2])
+    )
+    print("|{}{:^44}{}|".format(
+        style[1], "MASS EFFECT 2 Coalesced Options", style[2])
     )
     print("|{}{:^26}||{:^16}{}|".format(
-        style[0], headers[0], headers[1], style[1])
+        style[0], headers[0], headers[1], style[2])
     )
     # Print rows
     for selection, coa in enumerate(coalFiles, start=1):
@@ -90,4 +107,4 @@ def printtable():
             print(style[0], end='')
         print("|{:^26}||{:^16}|".format(coa, selection))
         if selection == 5:
-            print(style[1])
+            print(style[2])
